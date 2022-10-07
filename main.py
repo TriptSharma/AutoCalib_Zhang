7cf44d939c473a5435443cb0b29869eec23656cb
TriptSharma <tript35.sharma@gmail.com>
2022-10-07 19:55:58 -0400

    autocalib repo init

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import cv2
+import os
+
+images = os.listdir('Calibration_Imgs')
+print(images)
+
+for img in images:
+    #get m from images
+    #create M
+    #find homography
+    #get V
+    #get b
+    #get B
+    #get A
